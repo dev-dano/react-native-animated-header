9d85cd6e8db5e61c1e37b61b9318e21b950fbb8d
dev-dano <42421461+dev-dano@users.noreply.github.com>
2018-08-15 23:27:48 -0400

    Update contactList.js
    Set up

--- src/containers/contactList.js
+++ src/containers/contactList.js
@@ -1,1 +1,43 @@
+import React from 'react';
+import {
+  View,
+  FlatList,
+  StyleSheet
+} from 'react-native';
 
+import ContactListItem from '../components/contactListItem';
+
+
+export default class contactList extends React.Component {
+  state = {};
+  
+  _keyExtractor = (item, index) => {
+  
+  };
+
+  _renderItem = ({ item }) => {
+    
+  };
+   
+  render()  {
+    return (
+      <View style={styles.container}>
+        // Search bar
+      
+        <FlatList
+          keyExtractor={this._keyExtractor}
+          renderItem={this._renderItem}
+        />
+      </View>
+    );   
+  }
+  
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    
+  }
+  
+});
