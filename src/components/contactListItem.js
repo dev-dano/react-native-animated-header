cb120dd55657ab1308062fe8f0b2748c04af29cb
dev-dano <42421461+dev-dano@users.noreply.github.com>
2018-08-15 23:12:56 -0400

    Update contactListItem.js
    ListItem will always re-render when its parent component renders. To avoid unnecessary renders, use PureComponent (shallow prop comparison).

--- src/components/contactListItem.js
+++ src/components/contactListItem.js
@@ -1,1 +1,9 @@
+import React from 'react';
+import { ListItem } from 'react-native-elements';
 
+// Use PureComponent to avoid unnecessary render.
+export default class contactListItem extends React.PureComponent {
+  render()  {
+    return <ListItem {...this.props} />;
+  }
+}
