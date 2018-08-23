ba25c517dd116405c64eb0ee7e0e6aca258fbff5
dev-dano <42421461+dev-dano@users.noreply.github.com>
2018-08-23 14:44:07 -0400

    Update heightPropType.js

--- src/customPropTypes/heightPropType.js
+++ src/customPropTypes/heightPropType.js
@@ -1,1 +1,23 @@
+const heightPropType = (props, propName, componentName) => {
+	const prop = props[propName];
+	const propSupplied = (prop !== undefined);
+	const propIsNaN = isNaN(props[propName]);
+	const childrenSupplied = (props['children'] !== undefined);
 
+	if (childrenSupplied && propSupplied)
+		if (propIsNaN)
+			return new Error(
+				'Invalid prop `' + propName + '` supplied to' +
+				' `' + componentName + '`. Validation failed.'
+			);
+  else if (childrenSupplied)
+			return new Error(
+				'`' + propName + '`is required with prop `children`.'
+			);
+	else if (propSupplied)
+			return new Error(
+				'`' + propName + '`is only required with prop `children` supplied.'
+			);
+}
+
+export default heightPropType;
